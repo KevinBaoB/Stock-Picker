777a1b788cc46a8d687bd442c2df047759639dbb
KevinBaoB <kevinbaobui@gmail.com>
2022-06-06 11:45:30 -0500

    First

--- python/stock_picker.py
+++ python/stock_picker.py
@@ -1,2 +1,11 @@
 def picker(prices):
-    pass
+    highestRes = 0;
+    highestPair = [];
+    for i in range(len(prices)):
+        for j in range(i + 1, len(prices)):
+            result = prices[j] - prices[i];
+            if result > highestRes:
+                highestRes = result;
+                highestPair = [i, j]
+      
+    return highestPair;
