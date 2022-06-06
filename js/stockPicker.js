ed2cc6b5221983e7c3d4826eae82af4e51a14929
KevinBaoB <kevinbaobui@gmail.com>
2022-06-06 11:40:19 -0500

    First

--- js/stockPicker.js
+++ js/stockPicker.js
@@ -1,3 +1,14 @@
-exports.picker = function(prices) {
-
-}
+exports.picker = function (prices) {
+  let highestRes = 0;
+  let highestPair = [];
+  for (let i = 0; i < prices.length; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      let result = prices[j] - prices[i];
+      if (result > highestRes) {
+        highestRes = result;
+        highestPair = [i, j];
+      }
+    }
+  }
+  return highestPair;
+};
